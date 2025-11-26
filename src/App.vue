bb1a8956067ad2a2285381f04a9059ba41692197
Rxak22 <maboisakurak77@gmail.com>
2025-11-26 22:57:01 +0700

    update project card with new glowing hover, add thumbnail, ...

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <Hero />
     <About />
     <Skills />
-    <section id="projects">
+    <section id="projects" class="flex w-[80%] flex-wrap gap-2 mx-auto justify-center">
       <ProjectCard v-for="project in projects" :key="project.name" :project="project" />
     </section>
     <Blog />
@@ -25,7 +25,78 @@ import Contact from './components/Contact.vue'
 import Footer from './components/Footer.vue'
 
 const projects = ref([
-  { name: 'Phone Shop', tech: 'Vue + ASP.NET', repo: '#', demo: '#' },
-  { name: 'Todo App', tech: 'Vue 3 + Tailwind', repo: '#', demo: '#' }
+  {
+    name: "School Management System",
+    tech: "Laravel 9 + Bootstrap + MySQL",
+    repo: "https://github.com/Rxak22/school-mgt-system",
+    demo: "#",
+    featured: true,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764168752/c9901c97-d241-4767-be5e-bd56beefad54.png"
+  },
+  {
+    name: "Shop | Api Project",
+    tech: "Vue 3 + TypeScript + Tailwind CSS + Fake Api",
+    repo: "https://github.com/Rxak22/shop-project",
+    demo: "#",
+    featured: false,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764169434/ceed58a8-d050-4757-b4f0-1bd90c33fc68.png"
+  },
+  {
+    name: "Todo App",
+    tech: "Vue 3 + TypeScript + Tailwind CSS",
+    repo: "https://github.com/Rxak22/todo-app",
+    demo: "#",
+    featured: false,
+    thumbnail: ""
+  },
+  {
+    name: "Todo Api",
+    tech: "ASP.NET Core + SQL Server",
+    repo: "https://github.com/Rxak22/todoApi",
+    demo: "#",
+    featured: false,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764169839/797189df-18df-4cdc-ba01-ef463b2f018f.png"
+  },
+  {
+    name: "Attendence Management System",
+    tech: "Laravel + Bootstrap + MySQL",
+    repo: "https://github.com/Rxak22/attendence_mgt_sys",
+    demo: "#",
+    featured: false,
+    thumbnail: ""
+  },
+  {
+    name: "News Website",
+    tech: "PHP + MySQL + Bootstrap",
+    repo: "https://github.com/Rxak22/News-Website-Full-Stack",
+    demo: "#",
+    featured: false,
+    thumbnail: ""
+  },
+  {
+    name: "Book Store",
+    tech: "React Js + Tailwind CSS",
+    repo: "https://github.com/Rxak22/Book-Store",
+    demo: "#",
+    featured: false,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764170743/af3afa70-154e-4d5b-829c-47451fc2c519.png"
+  },
+  {
+    name: "SMTP Email Sender",
+    tech: "Laravel 9 + Bootstrap",
+    repo: "https://github.com/Rxak22/laravel-smtp",
+    demo: "#",
+    featured: false,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764171100/c4d10eb4-3a7d-4581-bad7-03150b350d23.png"
+  },
+  {
+    name: "Sanctum API",
+    tech: "Laravel 9 + Bootstrap",
+    repo: "https://github.com/Rxak22/laravel-sanctum-api",
+    demo: "#",
+    featured: false,
+    thumbnail: "https://res.cloudinary.com/dvsqwcz7u/image/upload/v1764171551/9c638eb9-1a83-413a-86bd-7e28f58660f3.png"
+  },
 ])
+
 </script>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="bg-black/70 p-6 rounded-xl shadow-lg transform transition-all hover:-translate-y-4 hover:scale-105 neon-border w-72" data-aos="fade-up">
-    <h3 class="text-xl font-bold neon-text mb-2">{{ project.name }}</h3>
-    <p class="text-sm text-white mb-4">{{ project.tech }}</p>
+  <div class="relative group bg-black/60 backdrop-blur-xl p-5 rounded-2xl shadow-lg w-80 border border-white/10 
+        transition-all duration-300 
+        hover:-translate-y-3 hover:shadow-[0_0_25px_rgba(0,255,255,0.4)]" data-aos="fade-up">
+
+        <!-- Thumbnail -->
+    <div class="w-full h-40 rounded-xl overflow-hidden mb-4">
+      <img
+        :src="project.thumbnail"
+        alt="project thumbnail"
+        class="w-full h-full object-cover group-hover:scale-105 transition"
+      />
+    </div>
+    
+    <h3 class="text-xl font-bold neon-text mb-1">{{ project.name }}</h3>
+    <p class="text-sm text-gray-300 mb-4">{{ project.tech }}</p>
     <div class="flex justify-between">
       <a :href="project.repo" target="_blank" class="text-cyan-400 hover:underline">Repo</a>
       <a :href="project.demo" target="_blank" class="text-cyan-400 hover:underline">Demo</a>
     </div>
+
+    <!-- Animated Border -->
+    <div class="absolute inset-0 rounded-2xl border border-cyan-400/0 
+           group-hover:border-cyan-400/40 transition-all duration-300 pointer-events-none">
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: ["project"]
-}
+<script setup>
+  defineProps({
+    project: {
+      tyoe: Object,
+      require: true
+    }
+  })
 </script>
 
 <style scoped>
@@ -21,5 +41,8 @@ export default {
   border: 1px solid rgba(255,255,255,0.06);
   box-shadow: none;
   background-color: rgba(255,255,255,0.03);
+}
+.neon-text {
+  text-shadow: 0 0 3px cyan, 0 0 6px cyan;
 }
 </style>
