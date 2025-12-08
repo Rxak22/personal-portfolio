172729e6d41f78d99c1801a9b084133e1d866e3c
Rxak22 <maboisakurak77@gmail.com>
2025-12-08 08:20:45 +0700

    Restore local main.js, styles.css

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-black/80 py-8 mt-20 text-center">
     <div class="flex justify-center space-x-6 mb-4">
       <!-- Empty social icons placeholders -->
-      <img src="../assets/social_placeholder.svg" alt="Social" class="w-8 h-8" v-for="n in 5" :key="n" />
+      <!-- <img src="../assets/social_placeholder.svg" alt="Social" class="w-8 h-8" v-for="n in 5" :key="n" /> -->
     </div>
     <p class="text-white text-sm">&copy; {{ new Date().getFullYear() }} Ouk Narak. All rights reserved.</p>
   </footer>
